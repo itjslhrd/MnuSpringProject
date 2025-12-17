396ba50c06d803f82f100f5d9078cfcf7176396f
websnet <websnet@DESKTOP-OMU8042>
2025-12-17 16:02:41 +0900

    12월17일

---
+++ exBoardMaven/src/main/java/com/mnu/exboard/ExBoardMavenApplication.java
@@ -0,0 +1,13 @@
+package com.mnu.exboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExBoardMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExBoardMavenApplication.class, args);
+	}
+
+}
---
+++ exBoardMaven/src/test/java/com/mnu/exboard/ExBoardMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnu.exboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExBoardMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
